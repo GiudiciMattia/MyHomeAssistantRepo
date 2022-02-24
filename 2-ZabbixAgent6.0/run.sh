3ce9a48e986bbf505dce8169a2250782fa1e3ada
GiudiciMattia <mattia.giudici@outlook.com>
2022-02-24 15:26:52 +0100

    11 commit zabbix

--- 2-ZabbixAgent6.0/run.sh
+++ 2-ZabbixAgent6.0/run.sh
@@ -20,9 +20,9 @@ sed -i 's/^#\?\s\?\(Hostname\)=.*$/\1='"${ZABBIX_HOSTNAME}"'/' "${ZABBIX_CONFIG_
 
 sed -i 's/^#\?\s\?\(TLSConnect\)=.*$/\1='"${ZABBIX_TLSCONNECT}"'/' "${ZABBIX_CONFIG_FILE}"
 sed -i 's/^#\?\s\?\(TLSAccept\)=.*$/\1='"${ZABBIX_TLSACCEPT}"'/' "${ZABBIX_CONFIG_FILE}"
-sed -i 's/^#\?\s\?\(TLSPSKIdentity\)=.*$/\1='"${ZABBIX_TLSPSKIDENTITY}"'/' "${ZABBIX_CONFIG_FILE}"
-sed -i 's/^#\?\s\?\(TLSPSKFile\)=.*$/\1='"${ZABBIX_PSKKEYFILE}"'/' "${ZABBIX_CONFIG_FILE}"
 
+echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
+echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> "${ZABBIX_CONFIG_FILE}"
 
 # Run zabbix-agent2 in foreground
 exec su zabbix -s /bin/ash -c "zabbix_agent2 -f"
