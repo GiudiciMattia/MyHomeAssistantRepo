b3939c7de3c46753539855eeba6768ab4623df88
GiudiciMattia <mattia.giudici@outlook.com>
2022-02-24 14:43:00 +0100

    terzo commit zabbix

--- 2-ZabbixAgent6.0/run.sh
+++ 2-ZabbixAgent6.0/run.sh
@@ -1,5 +1,14 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env ash
 
-echo "Hello world!"
+# Extract config data
+CONFIG_PATH=/data/options.json
+ZABBIX_SERVER=$(jq --raw-output ".server" "$CONFIG_PATH")
+ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
 
-python3 -m http.server 8000
+# Update zabbix-agent config
+ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agent2.conf
+sed -i 's@^#\?\s\?\(Server\(Active\)\?\)=.*@\1='"${ZABBIX_SERVER}"'@' "$ZABBIX_CONFIG_FILE"
+sed -i 's/^#\?\s\?\(Hostname\)=.*$/\1='"${ZABBIX_HOSTNAME}"'/' "${ZABBIX_CONFIG_FILE}"
+
+# Run zabbix-agent2 in foreground
+exec su zabbix -s /bin/ash -c "zabbix_agent2 -f"
