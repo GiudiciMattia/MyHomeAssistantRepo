f4b8a52b31de80982f980090ea1cf72e27e08d34
GiudiciMattia <mattia.giudici@outlook.com>
2022-02-25 18:55:38 +0100

    Revert "2 commit 1.8"
    This reverts commit 2800d92016b0942ad3b547fc3acaf36a5f920581.

--- 2-ZabbixAgent6.0/run.sh
+++ 2-ZabbixAgent6.0/run.sh
@@ -2,36 +2,80 @@
 
 # Extract config data
 CONFIG_PATH=/data/options.json
-ZABBIX_REMCOMM=$(jq --raw-output ".EnableRemoteCommands" "$CONFIG_PATH")
 ZABBIX_SERVER=$(jq --raw-output ".server" "$CONFIG_PATH")
-ZABBIX_SERVERACTIVE=$(jq --raw-output ".server_active" "$CONFIG_PATH")
 ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
-ZABBIX_STARTAGENT=$(jq --raw-output ".StartAgents" "$CONFIG_PATH")
-ZABBIX_REFRESHACTIVECHECKS=$(jq --raw-output ".RefreshActiveChecks" "$CONFIG_PATH")
-ZABBIX_BUFFERSEND=$(jq --raw-output ".BufferSend" "$CONFIG_PATH")
-ZABBIX_BUFFERSIZE=$(jq --raw-output ".BufferSize" "$CONFIG_PATH")
-ZABBIX_TIMEOUT=$(jq --raw-output ".Timeout" "$CONFIG_PATH")
 
 ZABBIX_TLSCONNECT=$(jq --raw-output ".TLSConnect" "$CONFIG_PATH")
 ZABBIX_TLSACCEPT=$(jq --raw-output ".TLSAccept" "$CONFIG_PATH")
 ZABBIX_TLSPSKIDENTITY=$(jq --raw-output ".TLSPSKIdentity" "$CONFIG_PATH")
 ZABBIX_TLSPSKCODE=$(jq --raw-output ".TLSPSKCode" "$CONFIG_PATH")
-
 ZABBIX_PSKKEYFILE="/etc/zabbix/zabbix_agent.psk"
+
 echo ${ZABBIX_TLSPSKCODE} > "/etc/zabbix/zabbix_agent.psk"
 
 # Update zabbix-agent config
 ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agentd.conf
 
-echo "EnableRemoteCommands=$ZABBIX_REMCOMM" >> "${ZABBIX_CONFIG_FILE}"
-echo "Server=$ZABBIX_SERVER" >> "${ZABBIX_CONFIG_FILE}"
-echo "ServerActive=$ZABBIX_SERVERACTIVE" >> "${ZABBIX_CONFIG_FILE}"
-echo "Hostname=$ZABBIX_HOSTNAME" >> "${ZABBIX_CONFIG_FILE}"
-echo "StartAgents=$ZABBIX_STARTAGENT" >> "${ZABBIX_CONFIG_FILE}"
-echo "RefreshActiveChecks=$ZABBIX_REFRESHACTIVECHECKS" >> "${ZABBIX_CONFIG_FILE}"
-echo "BufferSend=$ZABBIX_BUFFERSEND" >> "${ZABBIX_CONFIG_FILE}"
-echo "BufferSize=$ZABBIX_BUFFERSIZE" >> "${ZABBIX_CONFIG_FILE}"
-echo "Timeout=$ZABBIX_TIMEOUT" >> "${ZABBIX_CONFIG_FILE}"
+
+# # # EnableRemoteCommands=1
+
+# # # ##### Passive checks related
+# # # Server=zabbix-proxy-1.winsite.mshome.local
+
+# # # ### Option: ListenPort
+# # # ListenPort=10050
+
+# # # ### Option: ListenIP
+# # # ListenIP=0.0.0.0
+
+# # # ### Option: StartAgents
+# # # StartAgents=3
+
+# # # ### Option: ServerActive
+# # # ServerActive=zabbix-proxy-1.winsite.mshome.local
+
+# # # ### Option: Hostname
+# # # Hostname=DCBSX001.winsite.mshome.local
+
+# # # ### Option: RefreshActiveChecks
+# # # RefreshActiveChecks=120
+
+# # # ### Option: BufferSend
+# # # BufferSend=5
+
+# # # ### Option: BufferSize
+# # # BufferSize=100
+
+# # # ### Option: Timeout
+# # # Timeout=3
+
+# # # TLSConnect=psk
+# # # TLSAccept=psk
+# # # TLSPSKIdentity=PSK001
+# # # TLSPSKFile=/etc/zabbix/zabbix_agent.psk
+
+
+
+
+
+
+
+
+
+
+
+# sed -i 's@^#\?\s\?\(Server\(Active\)\?\)=.*@\1='"${ZABBIX_SERVER}"'@' "$ZABBIX_CONFIG_FILE"
+# sed -i 's/^#\?\s\?\(Hostname\)=.*$/\1='"${ZABBIX_HOSTNAME}"'/' "${ZABBIX_CONFIG_FILE}"
+# sed -i 's/^#\?\s\?\(TLSConnect\)=.*$/\1='"${ZABBIX_TLSCONNECT}"'/' "${ZABBIX_CONFIG_FILE}"
+# sed -i 's/^#\?\s\?\(TLSAccept\)=.*$/\1='"${ZABBIX_TLSACCEPT}"'/' "${ZABBIX_CONFIG_FILE}"
+# echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
+# echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> "${ZABBIX_CONFIG_FILE}"
+#echo "ControlSocket=/run/zabbix/zabbix_agent2.sock" >> "${ZABBIX_CONFIG_FILE}"
+
+echo "Hostname=srvlnxhassio01.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+echo "LogFile=/var/log/zabbix/zabbix_agent2.log" >> "${ZABBIX_CONFIG_FILE}"
+echo "Server=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+echo "ServerActive=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSAccept=psk" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSConnect=psk" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
