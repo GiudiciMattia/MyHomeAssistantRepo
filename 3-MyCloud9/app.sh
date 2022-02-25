e1b213cd322eb3cc11e03f9ece987c1dc503cdfa
GiudiciMattia <mattia.giudici@outlook.com>
2022-02-25 09:08:31 +0100

    18

--- 3-MyCloud9/app.sh
+++ 3-MyCloud9/app.sh
@@ -10,6 +10,6 @@ else
 fi
 
 cd /c9
-exec node /c9/server.js --listen 0.0.0.0 --port 8321 -w /config ${C9AUTH}
+exec node /c9/server.js --listen 0.0.0.0 --port 80 -w /config ${C9AUTH}
 
 # >>/log/c9.log 2>>/log/c9.err
