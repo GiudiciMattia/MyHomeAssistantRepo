676a803b57bc3fa8cfb406faba7c2b2150d2299d
GiudiciMattia <mattia.giudici@outlook.com>
2022-02-26 14:20:52 +0100

    2.9 commit zabbix

--- 2-ZabbixAgent6.0/run.sh
+++ 2-ZabbixAgent6.0/run.sh
@@ -5,29 +5,19 @@ CONFIG_PATH=/data/options.json
 ZABBIX_SERVER=$(jq --raw-output ".server" "$CONFIG_PATH")
 ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
 
-ZABBIX_TLSCONNECT=$(jq --raw-output ".TLSConnect" "$CONFIG_PATH")
-ZABBIX_TLSACCEPT=$(jq --raw-output ".TLSAccept" "$CONFIG_PATH")
-ZABBIX_TLSPSKIDENTITY=$(jq --raw-output ".TLSPSKIdentity" "$CONFIG_PATH")
-ZABBIX_TLSPSKCODE=$(jq --raw-output ".TLSPSKCode" "$CONFIG_PATH")
-ZABBIX_PSKKEYFILE="/etc/zabbix/zabbix_agent.psk"
-
-echo ${ZABBIX_TLSPSKCODE} > "/etc/zabbix/zabbix_agent.psk"
+echo 12c8d3be3ed7284614e0a1c7f33b74e4 > "/etc/zabbix/zabbix_agent.psk"
 
 # Update zabbix-agent config
 ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agentd.conf
 
-# echo "Hostname=srvlnxhassio01.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
-# echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> "${ZABBIX_CONFIG_FILE}"
-# echo "Server=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
-# echo "ServerActive=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
-# echo "TLSAccept=psk" >> "${ZABBIX_CONFIG_FILE}"
-# echo "TLSConnect=psk" >> "${ZABBIX_CONFIG_FILE}"
-# echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
-# echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> "${ZABBIX_CONFIG_FILE}"
-
-
-
-
+echo "Hostname=srvlnxhassio01.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> "${ZABBIX_CONFIG_FILE}"
+echo "Server=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+echo "ServerActive=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+echo "TLSAccept=psk" >> "${ZABBIX_CONFIG_FILE}"
+echo "TLSConnect=psk" >> "${ZABBIX_CONFIG_FILE}"
+echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
+echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> "${ZABBIX_CONFIG_FILE}"
 
 # Run zabbix-agent2 in foreground
 exec su zabbix -s /bin/ash -c "zabbix_agentd -f"
