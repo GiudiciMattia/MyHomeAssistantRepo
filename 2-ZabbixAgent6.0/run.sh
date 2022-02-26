853e686bb3f508b15c32a905e983ee63fd2c130d
GiudiciMattia <mattia.giudici@outlook.com>
2022-02-26 14:16:57 +0100

    2.8 zabbix commit

--- 2-ZabbixAgent6.0/run.sh
+++ 2-ZabbixAgent6.0/run.sh
@@ -16,14 +16,18 @@ echo ${ZABBIX_TLSPSKCODE} > "/etc/zabbix/zabbix_agent.psk"
 # Update zabbix-agent config
 ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agentd.conf
 
-echo "Hostname=srvlnxhassio01.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
-echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> "${ZABBIX_CONFIG_FILE}"
-echo "Server=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
-echo "ServerActive=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
-echo "TLSAccept=psk" >> "${ZABBIX_CONFIG_FILE}"
-echo "TLSConnect=psk" >> "${ZABBIX_CONFIG_FILE}"
-echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
-echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> "${ZABBIX_CONFIG_FILE}"
+# echo "Hostname=srvlnxhassio01.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+# echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> "${ZABBIX_CONFIG_FILE}"
+# echo "Server=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+# echo "ServerActive=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+# echo "TLSAccept=psk" >> "${ZABBIX_CONFIG_FILE}"
+# echo "TLSConnect=psk" >> "${ZABBIX_CONFIG_FILE}"
+# echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
+# echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> "${ZABBIX_CONFIG_FILE}"
+
+
+
+
 
 # Run zabbix-agent2 in foreground
 exec su zabbix -s /bin/ash -c "zabbix_agentd -f"
