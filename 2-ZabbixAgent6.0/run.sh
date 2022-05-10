010b42954d14bc4bae04a96e30ce5f533a8a2a61
GiudiciMattia <mattia.giudici@outlook.com>
2022-05-10 10:20:47 +0200

    aggiunta dns

--- 2-ZabbixAgent6.0/run.sh
+++ 2-ZabbixAgent6.0/run.sh
@@ -4,6 +4,7 @@
 CONFIG_PATH=/data/options.json
 ZABBIX_SERVER=$(jq --raw-output ".server" "$CONFIG_PATH")
 ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
+NAME_SERVER=$(jq --raw-output ".nameserver" "$CONFIG_PATH")
 
 echo 12c8d3be3ed7284614e0a1c7f33b74e4 > "/etc/zabbix/zabbix_agent.psk"
 
@@ -18,6 +19,8 @@ echo "TLSAccept=psk" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSConnect=psk" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> "${ZABBIX_CONFIG_FILE}"
+
+echo "nameserver ${NAME_SERVER}" > "/etc/resolv.conf"
 
 # Run zabbix-agent2 in foreground
 exec su zabbix -s /bin/ash -c "zabbix_agentd -f"
