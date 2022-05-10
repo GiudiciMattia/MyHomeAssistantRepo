2294b377bab52a29d775688e83a627dd702ebe1b
GiudiciMattia <mattia.giudici@outlook.com>
2022-05-10 09:23:05 +0200

    ultimo aggiornamento

--- 2-ZabbixAgent6.0/run.sh
+++ 2-ZabbixAgent6.0/run.sh
@@ -10,10 +10,10 @@ echo 12c8d3be3ed7284614e0a1c7f33b74e4 > "/etc/zabbix/zabbix_agent.psk"
 # Update zabbix-agent config
 ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agentd.conf
 
-echo "Hostname=srvlnxhassio01.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+echo "Hostname=${ZABBIX_HOSTNAME}" >> "${ZABBIX_CONFIG_FILE}"
 echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> "${ZABBIX_CONFIG_FILE}"
-echo "Server=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
-echo "ServerActive=zabbix-proxy-1.winsite.mshome.local" >> "${ZABBIX_CONFIG_FILE}"
+echo "Server=${ZABBIX_SERVER}" >> "${ZABBIX_CONFIG_FILE}"
+echo "ServerActive=${ZABBIX_SERVER}" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSAccept=psk" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSConnect=psk" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
