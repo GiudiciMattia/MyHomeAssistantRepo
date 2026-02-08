c39fd0fbcdd3e76ece8d38fe4f9a2522a8f74c83
GiudiciMattia <mattia.giudici@outlook.com>
2026-02-08 17:22:58 +0100

    a modo mio 1

--- 4-ZabbixAgent7.0/run.sh
+++ 4-ZabbixAgent7.0/run.sh
@@ -6,11 +6,11 @@ echo "=== Zabbix Agent 7 Add-on Startup ==="
 
 CONFIG_FILE="/data/options.json"
 # Leggiamo options.json come root (qui lo script gira come root)
-SERVER=$(jq -r '.server // empty' "$CONFIG_FILE")
-SERVER_ACTIVE=$(jq -r '.server_active // empty' "$CONFIG_FILE")
-HOSTNAME=$(jq -r '.hostname // empty' "$CONFIG_FILE")
-PSK_IDENTITY=$(jq -r '.psk_identity // empty' "$CONFIG_FILE")
-PSK_SECRET=$(jq -r '.psk_secret // empty' "$CONFIG_FILE")
+ZABBIX_SERVER=$(jq -r '.server // empty' "$CONFIG_FILE")
+ZABBIX_SERVER_ACTIVE=$(jq -r '.server_active // empty' "$CONFIG_FILE")
+ZABBIX_HOSTNAME=$(jq -r '.hostname // empty' "$CONFIG_FILE")
+ZABBIX_PSK_IDENTITY=$(jq -r '.psk_identity // empty' "$CONFIG_FILE")
+ZABBIX_PSK_PSK_SECRET=$(jq -r '.psk_secret // empty' "$CONFIG_FILE")
 
 echo "Loaded configuration:"
 echo "  Server: $SERVER"
@@ -23,11 +23,11 @@ ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agentd.conf
 echo "Hostname=${ZABBIX_HOSTNAME}" >> "${ZABBIX_CONFIG_FILE}"
 echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> "${ZABBIX_CONFIG_FILE}"
 echo "Server=${ZABBIX_SERVER}" >> "${ZABBIX_CONFIG_FILE}"
-echo "ServerActive=${ZABBIX_SERVER}" >> "${ZABBIX_CONFIG_FILE}"
+echo "ServerActive=${ZABBIX_SERVER_ACTIVE}" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSAccept=psk" >> "${ZABBIX_CONFIG_FILE}"
 echo "TLSConnect=psk" >> "${ZABBIX_CONFIG_FILE}"
-echo "TLSPSKIdentity=PSK001" >> "${ZABBIX_CONFIG_FILE}"
-echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> "${ZABBIX_CONFIG_FILE}"
+echo "TLSPSKIdentity=${ZABBIX_PSK_IDENTITY}" >> "${ZABBIX_CONFIG_FILE}"
+echo "TLSPSKFile=${ZABBIX_PSK_PSK_SECRET}" >> "${ZABBIX_CONFIG_FILE}"
 
 # Run zabbix-agent2 in foreground
 exec su zabbix -s /bin/ash -c "zabbix_agentd -f"
